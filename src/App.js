9a6d29b2ca4cb91f49406a3278bf59b8378688ea
user <samuel_morgan_@outlook.com>
2023-03-10 16:53:37 -0500

    color in function

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ const App = () => {
     useEffect(() => {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
-        ctx.strokeStyle = "#ccc";
         let i = 4.5;
         const intervalId = setInterval(() => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
